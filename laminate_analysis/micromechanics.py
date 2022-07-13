83ba107cf164a64afba8add5447086a969ab6634
Dmytro <kuksenko.dima@gmail.com>
2022-07-13 16:31:50 -0600

    added TODO to micromechanics py

--- laminate_analysis/micromechanics.py
+++ laminate_analysis/micromechanics.py
@@ -1,5 +1,12 @@
 """
-Homogeneous properties of a Unidirectional Lamina (Micromechanics). 
+Homogeneous properties of a unidirectional lamina.
+
+Estimation of effective properties usign semi-impirical methods [1].
+
+[1] I.Daniel, O.Ishai, Engineering Mechanics of Compsosite Materials. Oxford Univeristy Press, 2006.
+
+TODO: Add Mori-Tanaka homogenization scheme or other schemes for comparison
+TODO: Finite Element Method approach on a unit cell with hexagonal orientation of unidirectional fibers.
 
 Author: Dmytro Kuksenko
 """
