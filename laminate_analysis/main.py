4c87f24a2e8d7a4a5d0b8c239811675227263e56
Dmytro <kuksenko.dima@gmail.com>
2022-07-13 15:55:08 -0600

    added main window to the composite analysis tool

--- laminate_analysis/main.py
+++ laminate_analysis/main.py
@@ -4,6 +4,18 @@ Main file for the laminate analysis project.
 Author: Dmytro Kuksenko
 """
 
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget
+
 
 if __name__ == "__main__":
-    pass
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.resize(500, 300)
+    w.move(300, 300)
+    w.setWindowTitle("Composite Materials Analysis Tools")
+
+    w.show()
+
+    sys.exit(app.exec())
